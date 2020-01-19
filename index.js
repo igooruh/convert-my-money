ffe7894f410c3fb2096ddebebe142be858760fc9
Igor Pedro L M Martins <igooruh@icloud.com>
2020-01-18 22:16:19 -0300

    Task 003 - Create endpoint to quotation and validate of values

--- index.js
+++ index.js
@@ -9,6 +9,24 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.get('/', (req, res) => {
     res.render('home')
 })
+
+app.get('/cotacao', (req, res) => {
+    const {cotacao, quantidade} = req.query
+    if(cotacao && quantidade) {
+        const convertion = convert.convert(cotacao, quantidade)
+        res.render('cotacao', {
+            error: false,
+            cotacao: convert.toMoney(cotacao),
+            quantidade: convert.toMoney(quantidade),
+            convertion: convert.toMoney(convertion)
+        })
+    } else {
+        res.render('cotacao', {
+            error: 'Valores inválidos'
+        })
+    }
+})
+
 app.listen(3000, err => {
     err = err ? console.log('Convert my money offline') : console.log('Convert my money online')
     return err
